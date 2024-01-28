85dcfb76ddabff43b2536f8fa888577947124220
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-01-28 09:54:49 -0500

    Implement search feature

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,5 @@
-const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
-
-
 export const getArticles = (query) => {
-  return fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${REACT_APP_API_KEY}`)
-  .then((response) => {
-    if(response.ok){
-      return response.json()
-    }
-  })
-  .catch((err => console.error(err)))
-}
-
-export const getArticle = (query) => {
-  return fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${REACT_APP_API_KEY}`)
+  return fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.REACT_APP_API_KEY}`)
   .then((response) => {
     if(response.ok){
       return response.json()
