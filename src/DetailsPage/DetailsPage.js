e8b25857445066778fde92779a0551b003c00deb
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-01-28 10:32:10 -0500

    Add responsive design to details page

--- src/DetailsPage/DetailsPage.js
+++ src/DetailsPage/DetailsPage.js
@@ -9,11 +9,13 @@ function DetailsPage({ article }) {
       </div>
       <div className="details" key={article.id}>
         <h1> {article.name}</h1>
+        <div className="img-container">
         <img
           className="article-item image"
           src={article.urlToImage}
           alt={article.description}
         />
+        </div>
         <p className="article-item date">{article.publishedAt}</p>
         <p className="article-item content">{article.content}</p>
         <p className="article-item source">{article.source.name}</p>
